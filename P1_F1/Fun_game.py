22dba5a489794dffca9f9cf6b5c872285b66d0bb
Kamar <korpanlosa@gmail.com>
2024-10-19 09:27:56 +0200

    fun game

---
+++ P1_F1/Fun_game.py
@@ -0,0 +1,7 @@
+print("Create a character!")
+name = input("What's his name?")
+age = input("How old is He/she?")
+powers = input("What powers He/She has?")
+print("His/Her name is:", name)
+print("His/Her age is:", age, "year/s")
+print("His/Her powers are:", powers)
